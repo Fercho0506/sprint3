13e1a01c68d6500adcb61eac5302b1af21cd0a0f
Fercho0506 <157646134+Fercho0506@users.noreply.github.com>
2026-05-14 17:51:55 -0500

    Update urls.py

--- asr1_disponibilidad/app/urls.py
+++ asr1_disponibilidad/app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import HealthCheckView, AvailabilityMetricsView, MonthlyReportView
+from asr2_integridad.app.views import DataIngestView, IntegrityMetricsView
 
 urlpatterns = [
     path("health/", HealthCheckView.as_view(), name="health"),
     path("metrics/availability/", AvailabilityMetricsView.as_view(), name="availability_metrics"),
     path("reports/monthly/", MonthlyReportView.as_view(), name="monthly_report"),
+    path("cloud-data/ingest/", DataIngestView.as_view(), name="data_ingest"),
+    path("metrics/integrity/", IntegrityMetricsView.as_view(), name="integrity_metrics"),
 ]
